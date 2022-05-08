fa07a36cc245544b1dbbf934edf25316564f1492
huthoinguyn <huthoi99@gmail.com>
2022-05-08 23:45:39 +0700

    Layout

--- src/components/Layout/DefaultLayout/Sidebar/index.js
+++ src/components/Layout/DefaultLayout/Sidebar/index.js
@@ -1,5 +1,8 @@
+import classNames from 'classnames/bind';
+import Styles from './Sidebar.module.scss';
+const cx = classNames.bind(Styles);
 function Sidebar() {
-  return <h2>Sidebar</h2>;
+  return <aside className={cx('wrapper')}>Sidebar</aside>;
 }
 
 export default Sidebar;
